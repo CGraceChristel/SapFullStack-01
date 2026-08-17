d27a639c2ed4170b8734d98c8f624f24ac5d4c45
Grace Christel C <gracechristel.23cse@sonatech.ac.in>
2026-08-17 16:00:02 +0000

    Day01

---
+++ sapfullstack01/srv/service.cds
@@ -0,0 +1,8 @@
+@protocol:'rest'
+service product{
+    function printhelloworld(input: String) returns String;
+    function addition(num1:Integer, num2:Integer) returns Integer;
+    @open
+    type object{};
+    function myFunc(category:Integer) returns object;
+}
---
+++ sapfullstack01/srv/service.js
@@ -0,0 +1,37 @@
+const cds = require('@sap/cds');
+
+module.exports = cds.service.impl(async function (srv) {
+
+    srv.on('printhelloworld', req => {
+        console.log(req.data.input);
+        return `${req.data.input} World`;
+    });
+
+    srv.on('addition', req => {
+        console.log(req.data);
+
+        let result = req.data.num1 + req.data.num2;
+
+        return result;
+    });
+
+    srv.on('myFunc', req => {
+        let result = {};
+
+        if (req.data.category == 1) {
+            result.product = 'BMW';
+            result.price = '1200 USD';
+            result.location = 'New Delhi';
+        } else {
+            result.product = 'Audi';
+            result.stock = 120;
+            result.priceArray = {
+                Price: 475747,
+                Discount: '20.25'
+            };
+        }
+        console.log(result);
+        return result;
+    });
+
+});
